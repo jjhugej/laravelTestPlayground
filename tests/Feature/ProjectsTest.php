109ee56151c5c1ccd0e2bfd2057d6f645511606e
Jonathan <jonlillywebdev@gmail.com>
2020-05-07 19:42:32 -0400

    end of episode 3

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -24,4 +24,21 @@ class ProjectsTest extends TestCase
 
         $this->get('/projects')->assertSee($attributes['title']);
     }
+
+    public function test_a_project_requires_a_title(){
+
+        $attributes = factory('App\Project')->raw(['title' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    //instead of appending test_ to ever function you can do write the following directive 
+
+    public function test_a_project_requires_a_description(){
+
+        $attributes = factory('App\Project')->raw(['description' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+    }
+    
 }
